fcaf9e79764f70181dac831c45415b8acd3881df
Viliam Gago <viliam.gago95@gmail.com>
2026-01-03 07:25:31 +0100

    Add deployment infrastructure
    - Terraform config for ACA with Gradio
    - GitHub Actions workflow
    - Azure setup script

---
+++ terraform/container_app.tf
@@ -0,0 +1,108 @@
+resource "azurerm_container_app" "chatbot" {
+  name                         = "${var.project_name_no_dash}aca"
+  container_app_environment_id = azurerm_container_app_environment.c_app_env.id
+  resource_group_name          = data.azurerm_resource_group.app_rg.name
+  revision_mode                = "Single"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [local.app_identity_id]
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+
+  ingress {
+    external_enabled           = true
+    allow_insecure_connections = true
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+    target_port = 7860
+  }
+
+  registry {
+    server   = local.acr_login_server
+    identity = local.app_identity_id
+  }
+
+  # Secrets for sensitive values
+  secret {
+    name  = "sql-password"
+    value = local.sql_password
+  }
+
+  secret {
+    name  = "openai-api-key"
+    value = local.openai_api_key
+  }
+
+  template {
+    container {
+      name   = replace(var.image_name, "_", "")
+      image  = "${local.acr_login_server}/${var.image_name}:${var.environment}"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      # Azure OpenAI configuration
+      env {
+        name  = "AZURE_OPENAI_ENDPOINT"
+        value = local.openai_endpoint
+      }
+
+      env {
+        name  = "AZURE_OPENAI_DEPLOYMENT"
+        value = local.openai_deployment_name
+      }
+
+      env {
+        name        = "AZURE_OPENAI_API_KEY"
+        secret_name = "openai-api-key"
+      }
+
+      # SQL Database configuration
+      env {
+        name  = "AZURE_SQL_SERVER"
+        value = local.sql_server_fqdn
+      }
+
+      env {
+        name  = "AZURE_SQL_DATABASE"
+        value = local.sql_database_name
+      }
+
+      env {
+        name  = "SQL_USERNAME"
+        value = local.sql_username
+      }
+
+      env {
+        name        = "SQL_PASSWORD"
+        secret_name = "sql-password"
+      }
+
+      # Shared identity info (may be needed for future Azure SDK calls)
+      env {
+        name  = "AZURE_CLIENT_ID"
+        value = local.app_identity_client_id
+      }
+
+      env {
+        name  = "AZURE_TENANT_ID"
+        value = local.app_identity_tenant_id
+      }
+    }
+
+    min_replicas = 0
+    max_replicas = 1
+  }
+}
+
+output "chatbot_url" {
+  description = "URL of the deployed chatbot"
+  value       = "https://${azurerm_container_app.chatbot.ingress[0].fqdn}"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  backend "azurerm" {
+  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+# Reference core infrastructure from fin_az_core Terraform state
+data "terraform_remote_state" "core_infra" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = var.core_infra_rg
+    storage_account_name = var.core_infra_sa
+    container_name       = var.core_infra_container
+    key                  = var.core_infra_key
+    use_azuread_auth     = true
+  }
+}
+
+# Data source for fin_chatbot's OWN resource group
+data "azurerm_resource_group" "app_rg" {
+  name = var.rg_name
+}
+
+# Data source for Key Vault secrets
+data "azurerm_key_vault_secret" "sql_password" {
+  name         = "sql-app-password"
+  key_vault_id = data.terraform_remote_state.core_infra.outputs.key_vault_id
+}
+
+data "azurerm_key_vault_secret" "openai_api_key" {
+  name         = "openai-api-key"
+  key_vault_id = data.terraform_remote_state.core_infra.outputs.key_vault_id
+}
+
+# Local variables for core infrastructure references
+locals {
+  # App resource group
+  app_rg_name     = data.azurerm_resource_group.app_rg.name
+  app_rg_location = data.azurerm_resource_group.app_rg.location
+
+  # Core infrastructure references (from fin_az_core remote state)
+  key_vault_url    = data.terraform_remote_state.core_infra.outputs.key_vault_url
+  key_vault_id     = data.terraform_remote_state.core_infra.outputs.key_vault_id
+  acr_login_server = data.terraform_remote_state.core_infra.outputs.container_registry_login_server
+  acr_id           = data.terraform_remote_state.core_infra.outputs.container_registry_id
+
+  # SQL Database references (from fin_az_core remote state)
+  sql_server_fqdn   = data.terraform_remote_state.core_infra.outputs.sql_server_fqdn
+  sql_database_name = data.terraform_remote_state.core_infra.outputs.sql_database_name
+  sql_username      = "app_user"
+  sql_password      = data.azurerm_key_vault_secret.sql_password.value
+
+  # Azure OpenAI references (from fin_az_core remote state)
+  openai_endpoint        = data.terraform_remote_state.core_infra.outputs.openai_endpoint
+  openai_deployment_name = data.terraform_remote_state.core_infra.outputs.openai_deployment_name
+  openai_api_key         = data.azurerm_key_vault_secret.openai_api_key.value
+
+  # Shared application identity (from fin_az_core)
+  app_identity_id           = data.terraform_remote_state.core_infra.outputs.app_identity_id
+  app_identity_client_id    = data.terraform_remote_state.core_infra.outputs.app_identity_client_id
+  app_identity_principal_id = data.terraform_remote_state.core_infra.outputs.app_identity_principal_id
+  app_identity_tenant_id    = data.terraform_remote_state.core_infra.outputs.app_identity_tenant_id
+}
+
+resource "azurerm_container_app_environment" "c_app_env" {
+  name                = "${var.project_name_no_dash}cae"
+  location            = local.app_rg_location
+  resource_group_name = local.app_rg_name
+}
+
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
